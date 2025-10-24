81d0233ec33dd6fc1f23eea524bebc80834045fd
utsahice <utsahic.encircle@gmail.com>
2025-10-24 12:01:56 +0530

    style : tailwind css added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.set('views', path.join(__dirname, 'src/views'))
 
---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    plugins: {
+      '@tailwindcss/postcss': {}, // New required plugin for Tailwind 4.0
+      autoprefixer: {},
+    },
+  };
